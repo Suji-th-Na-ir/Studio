5e4a136e903790fa599076ee0f095b6255b2a313
Sujith:) <sujith@letsterra.com>
2023-08-01 15:43:39 +0530

    Sujith :) -> 1. Rotate component

---
+++ Assets/_Studio/_Runtime/Scripts/Components/RotateComponent.cs
@@ -0,0 +1,58 @@
+using Newtonsoft.Json;
+
+namespace Terra.Studio
+{
+    public struct RotateComponent : IBaseComponent, IConditional, IBroadcastData
+    {
+        public bool IsConditionAvailable { get; set; }
+        public string ConditionType { get; set; }
+        public string ConditionData { get; set; }
+        public bool IsBroadcastable { get; set; }
+        public string Broadcast { get; set; }
+        public bool IsTargeted { get; set; }
+        public int TargetId { get; set; }
+        [JsonIgnore] public bool CanExecute { get; set; }
+        [JsonIgnore] public bool IsExecuted { get; set; }
+
+        public Axis axis;
+        public Direction direction;
+        public RotationType rotationType;
+        public RepeatType repeatType;
+        public float speed;
+        public float rotateBy;
+        public float pauseFor;
+        public float repeatFor;
+        public bool canPlaySFX;
+        public string sfxName;
+        public int sfxIndex;
+        public bool canPlayVFX;
+        public string vfxName;
+        public int vfxIndex;
+
+        public enum Axis
+        {
+            X,
+            Y,
+            Z
+        }
+
+        public enum Direction
+        {
+            Clockwise,
+            AntiClockwise
+        }
+
+        public enum RotationType
+        {
+            Oscillate,
+            RotateOnce,
+            RotateMultipleTimes
+        }
+
+        public enum RepeatType
+        {
+            Forever,
+            XTimes
+        }
+    }
+}
