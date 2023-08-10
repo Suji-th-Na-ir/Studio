305b615411ea26d71d85e9e8c89fe203f7406853
Sujith:) <sujith@letsterra.com>
2023-08-10 11:26:43 +0530

    Sujith :) -> 1. Folder restructure

--- Assets/_Studio/_Core/Scripts/SystemConfigurationSO.cs
+++ Assets/_Studio/_Core/Scripts/SystemConfigurationSO.cs
@@ -8,6 +8,11 @@ namespace Terra.Studio
         [SerializeField] private StudioState defaultStudioState;
         [SerializeField] private string editorSceneName;
         [SerializeField] private string runtimeSceneName;
+
+        public StudioState DefaultStudioState { get { return defaultStudioState; } }
+        public string EditorSceneName { get { return editorSceneName; } }
+        public string RuntimeSceneName { get { return runtimeSceneName; } }
+
 #if UNITY_EDITOR
         [Space(10), Header("Editor only")]
         [SerializeField] private TextAsset SceneData;
@@ -17,9 +22,5 @@ namespace Terra.Studio
         [SerializeField] private bool loadDefaultSceneOnPlay;
         public bool LoadDefaultSceneOnPlay { get { return loadDefaultSceneOnPlay; } }
 #endif
-
-        public StudioState DefaultStudioState { get { return defaultStudioState; } }
-        public string EditorSceneName { get { return editorSceneName; } }
-        public string RuntimeSceneName { get { return runtimeSceneName; } }
     }
 }
