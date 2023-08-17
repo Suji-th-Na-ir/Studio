110f791c9f8ad921d91a4b8b2cfcd8ee7a24676f
Sujith:) <sujith@letsterra.com>
2023-08-17 18:19:20 +0530

    Sujith :) -> 1. Implemented In game timer

---
+++ Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/RtComponents/InGameTimer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Terra.Studio;
+using Newtonsoft.Json;
+
+namespace RuntimeInspectorNamespace
+{
+    [EditorDrawComponent("Terra.Studio.GameTimer")]
+    public class InGameTimer : MonoBehaviour, IComponent
+    {
+        public uint Time;
+        public string Broadcast;
+
+        public (string type, string data) Export()
+        {
+            GameTimerComponent component = new()
+            {
+                IsConditionAvailable = true,
+                ConditionType = "Terra.Studio.GameStart",
+                ConditionData = "OnStart",
+                IsBroadcastable = !string.IsNullOrEmpty(Broadcast),
+                Broadcast = Broadcast,
+                totalTime = Time
+            };
+            var type = EditorOp.Resolve<DataProvider>().GetCovariance(this);
+            var data = JsonConvert.SerializeObject(component);
+            return (type, data);
+        }
+
+        public void Import(EntityBasedComponent data)
+        {
+            var comp = JsonConvert.DeserializeObject<GameTimerComponent>($"{data.data}");
+            Time = comp.totalTime;
+            Broadcast = comp.Broadcast;
+        }
+    }
+}
