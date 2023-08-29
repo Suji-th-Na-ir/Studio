c16f71d030fd6a2b78b5544fea8fcce3ab9d4b48
Jay <jay@letsterra.com>
2023-08-29 17:29:23 +0530

    - Fixed the Hierarchy UI to take up full height. - Fixed the Inspector UI to take up full height. - Fixed the Scene Cam Viewport Gizmo to be placed properly in 16:9. 16:10 WIP.

--- Assets/_Studio/_Editor/Scripts/_ThirdParty/Runtime Transform Gizmos/Scripts/Gizmos/Scene Gizmo/SceneGizmoCamViewportUpdater.cs
+++ Assets/_Studio/_Editor/Scripts/_ThirdParty/Runtime Transform Gizmos/Scripts/Gizmos/Scene Gizmo/SceneGizmoCamViewportUpdater.cs
@@ -18,19 +18,22 @@ namespace RTG
             Rect sceneCameraViewRect = sceneGizmoCamera.SceneCamera.pixelRect;
             Vector2 camPrjSwitchLabelRectSize = lookAndFeel.CalculateMaxPrjSwitchLabelRectSize();
             bool usesPrjSwitchLabel = lookAndFeel.IsCamPrjSwitchLabelVisible;
+            lookAndFeel.ScreenSize = Screen.height / 10.8f;
             float screenSize = lookAndFeel.ScreenSize;
-
+            screenOffset.x = -Screen.width / 2.90002f;
+            screenOffset.y = -Screen.height / 10.8f;
             if (lookAndFeel.ScreenCorner == SceneGizmoScreenCorner.TopRight)
-                sceneGizmoCamera.Camera.pixelRect = new Rect(sceneCameraViewRect.xMax - screenSize + screenOffset.x, sceneCameraViewRect.yMax - screenSize + screenOffset.y, screenSize, screenSize);
+                //sceneGizmoCamera.Camera.pixelRect = new Rect ((sceneCameraViewRect.xMax / 2.90f) + screenSize, (sceneCameraViewRect.yMax / 10.08f) - screenSize, screenSize, screenSize);
+            sceneGizmoCamera.Camera.pixelRect = new Rect(sceneCameraViewRect.xMax - screenSize + screenOffset.x, sceneCameraViewRect.yMax - screenSize + screenOffset.y, screenSize, screenSize);
             else
             if (lookAndFeel.ScreenCorner == SceneGizmoScreenCorner.TopLeft)
-                sceneGizmoCamera.Camera.pixelRect = new Rect(sceneCameraViewRect.xMin + screenOffset.x, sceneCameraViewRect.yMax - screenSize + screenOffset.y, screenSize, screenSize);
+                sceneGizmoCamera.Camera.pixelRect = new Rect (sceneCameraViewRect.xMin + screenOffset.x, sceneCameraViewRect.yMax - screenSize + screenOffset.y, screenSize, screenSize);
             else
             if (lookAndFeel.ScreenCorner == SceneGizmoScreenCorner.BottomRight)
-                sceneGizmoCamera.Camera.pixelRect = new Rect(sceneCameraViewRect.xMax - screenSize + screenOffset.x,
+                sceneGizmoCamera.Camera.pixelRect = new Rect (sceneCameraViewRect.xMax - screenSize + screenOffset.x,
                     sceneCameraViewRect.yMin + (usesPrjSwitchLabel ? camPrjSwitchLabelRectSize.y + 1.0f : 0.0f) + screenOffset.y, screenSize, screenSize);
             else
-                sceneGizmoCamera.Camera.pixelRect = new Rect(sceneCameraViewRect.xMin + screenOffset.x,
+                sceneGizmoCamera.Camera.pixelRect = new Rect (sceneCameraViewRect.xMin + screenOffset.x,
                     sceneCameraViewRect.yMin + (usesPrjSwitchLabel ? camPrjSwitchLabelRectSize.y + 1.0f : 0.0f) + screenOffset.y, screenSize, screenSize);
         }
     }
