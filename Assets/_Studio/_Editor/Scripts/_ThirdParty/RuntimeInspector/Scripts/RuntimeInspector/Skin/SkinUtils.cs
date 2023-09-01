e122b5dff4467b84fc977d0ace6f1450210878c4
AkshatTerra <akshatpuri@letsterra.com>
2023-09-01 12:37:37 +0530

    Akshat-Fixed Complete rotate component UI

--- Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/Skin/SkinUtils.cs
+++ Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/Skin/SkinUtils.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using PlayShifu.Terra;
 
 namespace RuntimeInspectorNamespace
 {
@@ -19,7 +21,47 @@ namespace RuntimeInspectorNamespace
 			text.fontSize = skin.FontSize;
 		}
 
-		public static void SetSkinButtonText( this Text text, UISkin skin )
+        public static void SetSkinDropDownField(this Dropdown input, UISkin Skin)
+        {
+			var dropdownArrow = Helper.FindDeepChild(input.transform, "Arrow").GetComponent<Image>();
+			var templateContentTransform= Helper.FindDeepChild(input.transform, "Content").GetComponent<RectTransform>();
+			var templateItemTransform= Helper.FindDeepChild(input.transform, "Item").GetComponent<RectTransform>();
+			var templateText = input.itemText;
+			var background = input.GetComponent<Image>();
+			var templateCheckmark= Helper.FindDeepChild(input.transform, "Item Checkmark").GetComponent<Image>();
+			var templateBackground = input.template.GetComponent<Image>();
+			Vector2 templateContentSizeDelta = templateContentTransform.sizeDelta;
+            templateContentSizeDelta.y = Skin.LineHeight + 6f; // Padding at top and bottom edges
+            templateContentTransform.sizeDelta = templateContentSizeDelta;
+
+            Vector2 templateItemSizeDelta = templateItemTransform.sizeDelta;
+            templateItemSizeDelta.y = Skin.LineHeight;
+            templateItemTransform.sizeDelta = templateItemSizeDelta;
+
+            // Resize the checkmark icon
+            float templateCheckmarkSize = Skin.LineHeight * 0.66f;
+            Vector2 templateTextSizeDelta = templateText.rectTransform.sizeDelta;
+            templateTextSizeDelta.x -= templateCheckmarkSize - templateCheckmark.rectTransform.sizeDelta.x;
+            templateText.rectTransform.sizeDelta = templateTextSizeDelta;
+            templateCheckmark.rectTransform.sizeDelta = new Vector2(templateCheckmarkSize, templateCheckmarkSize);
+
+            // Resize the dropdown arrow
+            Vector2 dropdownTextSizeDelta = input.captionText.rectTransform.sizeDelta;
+            dropdownTextSizeDelta.x -= templateCheckmarkSize - dropdownArrow.rectTransform.sizeDelta.x;
+            input.captionText.rectTransform.sizeDelta = dropdownTextSizeDelta;
+            dropdownArrow.rectTransform.sizeDelta = new Vector2(templateCheckmarkSize, templateCheckmarkSize);
+
+            background.color = Skin.InputFieldNormalBackgroundColor;
+            dropdownArrow.color = Skin.TextColor.Tint(0.1f);
+
+            input.captionText.SetSkinInputFieldText(Skin);
+            templateText.SetSkinInputFieldText(Skin);
+
+            templateBackground.color = Skin.InputFieldNormalBackgroundColor.Tint(0.075f);
+            templateCheckmark.color = Skin.ToggleCheckmarkColor;
+        }
+
+        public static void SetSkinButtonText( this Text text, UISkin skin )
 		{
 			text.color = skin.ButtonTextColor;
 			text.font = skin.Font;
