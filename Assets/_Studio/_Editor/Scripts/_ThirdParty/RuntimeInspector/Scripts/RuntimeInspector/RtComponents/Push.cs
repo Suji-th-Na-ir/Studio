dbde302ae5b699efb6d16f0b488995c55046e177
Sujith:) <sujith@letsterra.com>
2023-09-04 10:50:54 +0530

    Sujith :) -> 1. Added push template for behaviour

---
+++ Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/RtComponents/Push.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using RuntimeInspectorNamespace;
+
+namespace Terra.Studio
+{
+    [EditorDrawComponent("Terra.Studio.Push")]
+    public class Push : MonoBehaviour, IComponent
+    {
+        public float drag = 0;
+        public bool showResetButton = true;
+        public Atom.PlaySfx playSFX;
+        public Atom.PlayVfx playVFX;
+
+        public (string type, string data) Export()
+        {
+            return default;
+        }
+
+        public void Import(EntityBasedComponent data)
+        {
+
+        }
+    }
+}
