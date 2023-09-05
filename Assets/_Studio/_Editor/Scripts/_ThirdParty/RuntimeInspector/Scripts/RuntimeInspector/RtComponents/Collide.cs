f2da34f6f3e1e7328468e640e2331ec9e4bbf38b
Sujith:) <sujith@letsterra.com>
2023-09-05 18:31:12 +0530

    Sujith :) -> 1. Implemented on collide template

---
+++ Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/RtComponents/Collide.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using Newtonsoft.Json;
+using RuntimeInspectorNamespace;
+
+namespace Terra.Studio
+{
+    [EditorDrawComponent("Terra.Studio.Collide")]
+    public class Collide : MonoBehaviour, IComponent
+    {
+        public Atom.Broadcast broadcast = new();
+        public Atom.PlaySfx playSFX = new();
+        public Atom.PlayVfx playVFX = new();
+        public bool executeMultipleTimes;
+
+        public (string type, string data) Export()
+        {
+            var data = new CollideComponent()
+            {
+                canPlaySFX = playSFX.data.canPlay,
+                sfxName = playSFX.data.clipName,
+                sfxIndex = playSFX.data.clipIndex,
+                canPlayVFX = playVFX.data.canPlay,
+                vfxName = playVFX.data.clipName,
+                vfxIndex = playVFX.data.clipIndex,
+                IsBroadcastable = !string.IsNullOrEmpty(broadcast.data.broadcastName),
+                Broadcast = broadcast.data.broadcastName,
+                broadcastIndex = broadcast.data.broadcastTypeIndex,
+                IsConditionAvailable = true,
+                ConditionType = "Terra.Studio.TriggerAction",
+                ConditionData = "Player",
+                listen = executeMultipleTimes ? Listen.Always : Listen.Once
+            };
+            var type = EditorOp.Resolve<DataProvider>().GetCovariance(this);
+            var json = JsonConvert.SerializeObject(data);
+            return (type, json);
+        }
+
+        public void Import(EntityBasedComponent data)
+        {
+            var obj = JsonConvert.DeserializeObject<CollideComponent>(data.data);
+            playSFX.data.canPlay = obj.canPlaySFX;
+            playSFX.data.clipName = obj.sfxName;
+            playSFX.data.clipIndex = obj.sfxIndex;
+            playVFX.data.canPlay = obj.canPlayVFX;
+            playVFX.data.clipName = obj.vfxName;
+            playVFX.data.clipIndex = obj.vfxIndex;
+            broadcast.data.broadcastName = obj.Broadcast;
+            broadcast.data.broadcastTypeIndex = obj.broadcastIndex;
+        }
+    }
+}
