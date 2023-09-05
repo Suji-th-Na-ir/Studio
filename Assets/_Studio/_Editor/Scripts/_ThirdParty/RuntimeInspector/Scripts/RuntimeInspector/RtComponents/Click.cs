e7eebe85e77aa9f082b9a5f83ab56597fec8edbb
Sujith:) <sujith@letsterra.com>
2023-09-05 18:47:41 +0530

    Sujith :) -> 1. Implemented click system

---
+++ Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/RtComponents/Click.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using Newtonsoft.Json;
+using RuntimeInspectorNamespace;
+
+namespace Terra.Studio
+{
+    [EditorDrawComponent("Terra.Studio.Click")]
+    public class Click : MonoBehaviour, IComponent
+    {
+        public Atom.Broadcast broadcast = new();
+        public Atom.PlaySfx playSFX = new();
+        public Atom.PlayVfx playVFX = new();
+        public bool executeMultipleTimes;
+
+        public (string type, string data) Export()
+        {
+            var data = new ClickComponent()
+            {
+                canPlaySFX = playSFX.data.canPlay,
+                sfxName = playSFX.data.clipName,
+                sfxIndex = playSFX.data.clipIndex,
+                canPlayVFX = playVFX.data.canPlay,
+                vfxName = playVFX.data.clipName,
+                vfxIndex = playVFX.data.clipIndex,
+                IsBroadcastable = !string.IsNullOrEmpty(broadcast.data.broadcastName),
+                Broadcast = broadcast.data.broadcastName,
+                broadcastIndex = broadcast.data.broadcastTypeIndex,
+                IsConditionAvailable = true,
+                ConditionType = "Terra.Studio.MouseAction",
+                ConditionData = "OnClick",
+                listen = executeMultipleTimes ? Listen.Always : Listen.Once
+            };
+            var type = EditorOp.Resolve<DataProvider>().GetCovariance(this);
+            var json = JsonConvert.SerializeObject(data);
+            return (type, json);
+        }
+
+        public void Import(EntityBasedComponent data)
+        {
+            var obj = JsonConvert.DeserializeObject<ClickComponent>(data.data);
+            playSFX.data.canPlay = obj.canPlaySFX;
+            playSFX.data.clipName = obj.sfxName;
+            playSFX.data.clipIndex = obj.sfxIndex;
+            playVFX.data.canPlay = obj.canPlayVFX;
+            playVFX.data.clipName = obj.vfxName;
+            playVFX.data.clipIndex = obj.vfxIndex;
+            broadcast.data.broadcastName = obj.Broadcast;
+            broadcast.data.broadcastTypeIndex = obj.broadcastIndex;
+            executeMultipleTimes = obj.listen == Listen.Always;
+        }
+    }
+}
