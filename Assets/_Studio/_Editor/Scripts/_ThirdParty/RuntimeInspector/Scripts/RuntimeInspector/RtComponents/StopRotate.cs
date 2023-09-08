2542a5a3b7d91ae9d318c10b1c1f89b4b9ddba6a
AkshatTerra <akshatpuri@letsterra.com>
2023-09-08 15:56:11 +0530

    Akshat-Fixed broken listen icons in some components

--- Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/RtComponents/StopRotate.cs
+++ Assets/_Studio/_Editor/Scripts/_ThirdParty/RuntimeInspector/Scripts/RuntimeInspector/RtComponents/StopRotate.cs
@@ -88,7 +88,10 @@ namespace Terra.Studio
             playVFX.data.canPlay = obj.canPlayVFX;
             playVFX.data.clipIndex = obj.vfxIndex;
             playVFX.data.clipName = obj.vfxName;
-            EditorOp.Resolve<UILogicDisplayProcessor>().ImportVisualisation(gameObject, GetType().Name, broadcast, startOn.data.listenName);
+            var listenString = "";
+            if (startOn.data.startIndex == 1)
+                listenString = startOn.data.listenName;
+            EditorOp.Resolve<UILogicDisplayProcessor>().ImportVisualisation(gameObject, GetType().Name, broadcast, listenString);
         }
 
         private StartOn GetStart(StopRotateComponent comp)
