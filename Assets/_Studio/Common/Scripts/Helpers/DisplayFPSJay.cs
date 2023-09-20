145367a9cf3319c366df82f0702c14acf664af1e
Jay <jay@letsterra.com>
2023-09-20 11:54:20 +0530

    - Marked all models are read-write enabled. - New scene. - Added FPS counter in Bootstrap.

---
+++ Assets/_Studio/Common/Scripts/Helpers/DisplayFPSJay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (Text))]
+public class DisplayFPSJay : MonoBehaviour {
+
+    private int FramesPerSec;
+    private float frequency = 1.0f;
+    private string fps;
+    private Text m_refText;
+    void Start () {
+        m_refText = this.gameObject.GetComponent<Text> ();
+        StartCoroutine (FPS ());
+    }
+
+    private IEnumerator FPS () {
+        for (; ; ) {
+            int lastFrameCount = Time.frameCount;
+            float lastTime = Time.realtimeSinceStartup;
+            yield return new WaitForSeconds (frequency);
+            float timeSpan = Time.realtimeSinceStartup - lastTime;
+            int frameCount = Time.frameCount - lastFrameCount;
+
+            fps = string.Format ("FPS: {0}", Mathf.RoundToInt (frameCount / timeSpan));
+            m_refText.text = fps;
+        }
+    }
+}
