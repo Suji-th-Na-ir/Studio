568208bb5123f40ee1c404806f298d41904ef531
Jay <jay@letsterra.com>
2023-09-25 20:21:54 +0530

    - UI revamp!!! - Added pref logic. - Lots of cleanup. - Added login success and login fail analytics.

--- Assets/_Studio/_Core/Scripts/PasswordManager.cs
+++ Assets/_Studio/_Core/Scripts/PasswordManager.cs
@@ -7,16 +7,30 @@ using UnityEngine;
 using UnityEngine.UI;
 using PlayShifu.Terra;
 using System.Collections;
+using mixpanel;
 
 namespace Terra.Studio
 {
     public class PasswordManager : MonoBehaviour
     {
         [SerializeField] private CanvasGroup m_refCanvasGroup;
+
+        [SerializeField] private InputField nameInputField;
+        [SerializeField] private Image m_refNameInputFieldBorder;
         [SerializeField] private InputField passwordInputField;
+        [SerializeField] private Image m_refPasswordInputFieldBorder;
+
         [SerializeField] private Text feedbackText;
-        [SerializeField] private Image m_refInputFieldBorder;
+        
         private const string correctPassword = "Terra@12345";
+
+        private string m_strName;
+        private string m_strPassword;
+
+        private bool m_bIsCheckPasswordInProgress;
+
+        [SerializeField] private GameObject m_refNotLoggedInScreenGO;
+        [SerializeField] private GameObject m_refLoggingYouInScreenGO;
 
         public event Action OnCorrectPasswordEntered;
 
@@ -26,16 +40,28 @@ namespace Terra.Studio
             feedbackText.text = "";
         }
 
-#if DEBUG
-        private IEnumerator Start()
-        {
-            yield return StartCoroutine(ShowCorrectPasswordAnim());
+        private void Start () {
+            if (PlayerPrefs.HasKey ("UserName")) {
+                m_strName = PlayerPrefs.GetString ("UserName");
+            }
+            if (PlayerPrefs.HasKey ("Password")) {
+                m_strPassword = PlayerPrefs.GetString ("Password");
+            }
+            if (m_strPassword == correctPassword) {
+                m_refLoggingYouInScreenGO.SetActive (true);
+                m_refNotLoggedInScreenGO.SetActive (false);
+                LogLoginEvent ("auto");
+                OnCorrectPasswordEntered?.Invoke ();
+            } else {
+                m_refLoggingYouInScreenGO.SetActive (false);
+                m_refNotLoggedInScreenGO.SetActive (true);
+            }
+            //yield return StartCoroutine (ShowCorrectPasswordAnim ());
         }
-#endif
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Return))
+            if (!m_bIsCheckPasswordInProgress && Input.GetKeyDown(KeyCode.Return))
             {
                 CheckPassword();
             }
@@ -43,14 +69,27 @@ namespace Terra.Studio
 
         public void CheckPassword()
         {
-            string enteredPassword = passwordInputField.text;
-            if (enteredPassword == correctPassword)
+            m_bIsCheckPasswordInProgress = true;
+            m_strName = nameInputField.text;
+            m_strPassword = passwordInputField.text;
+            if (string.IsNullOrEmpty(nameInputField.text)) {
+                LogLoginFailedEvent ("incorrect name");
+                m_bIsCheckPasswordInProgress = false;
+                StartCoroutine (ShowIncorrectNameAnim ("Please enter your name!"));
+                return;
+            }
+            if (m_strPassword == correctPassword)
             {
-                StartCoroutine(ShowCorrectPasswordAnim());
+                LogLoginEvent ("manual");
+                PlayerPrefs.SetString ("UserName", m_strName);
+                PlayerPrefs.SetString ("Password", m_strPassword);
+                StartCoroutine (ShowCorrectPasswordAnim());
             }
             else
             {
-                StartCoroutine(ShowIncorrectPasswordAnim("Incorrect Password. Please try again!"));
+                LogLoginFailedEvent ("incorrect password");
+                m_bIsCheckPasswordInProgress = false;
+                StartCoroutine (ShowIncorrectPasswordAnim("Incorrect Password. Please try again!"));
             }
         }
 
@@ -58,7 +97,7 @@ namespace Terra.Studio
         {
             float timer = 0f;
             Color toCol = Color.green;
-            Color fromCol = m_refInputFieldBorder.color;
+            Color fromCol = m_refPasswordInputFieldBorder.color;
             float speed = 3f;
             feedbackText.text = "Correct Password!";
             feedbackText.color = new Color(0, 0, 0, 0);
@@ -67,7 +106,8 @@ namespace Terra.Studio
             textToCol.a = 1f;
             do
             {
-                m_refInputFieldBorder.color = Color.Lerp(fromCol, toCol, timer);
+                m_refNameInputFieldBorder.color = Color.Lerp(fromCol, toCol, timer);
+                m_refPasswordInputFieldBorder.color = Color.Lerp(fromCol, toCol, timer);
                 feedbackText.color = Color.Lerp(textFromCol, textToCol, timer);
                 timer += Time.deltaTime * speed;
                 yield return new WaitForEndOfFrame();
@@ -75,11 +115,24 @@ namespace Terra.Studio
             OnCorrectPasswordEntered?.Invoke();
         }
 
+        private void LogLoginEvent (string a_strSource) {
+            Value val = new Value ();
+            val.Add ("username", m_strName);
+            val.Add ("source", a_strSource);
+            Mixpanel.Track ("LoginSuccessful", val);
+        }
+
+        private void LogLoginFailedEvent (string a_strReason) {
+            Value val = new Value ();
+            val.Add ("fail_reason", a_strReason);
+            Mixpanel.Track ("LoginFailed", val);
+        }
+
         private IEnumerator ShowIncorrectPasswordAnim(string a_strErrorValue)
         {
             float timer = 0f;
             Color toCol = Helper.GetColorFromHex("#E83811");
-            Color fromCol = m_refInputFieldBorder.color;
+            Color fromCol = m_refPasswordInputFieldBorder.color;
             float speed = 3f;
             feedbackText.text = a_strErrorValue;
             feedbackText.color = new Color(0, 0, 0, 0);
@@ -88,27 +141,65 @@ namespace Terra.Studio
             textToCol.a = 1f;
             do
             {
-                m_refInputFieldBorder.color = Color.Lerp(fromCol, toCol, timer);
+                m_refPasswordInputFieldBorder.color = Color.Lerp(fromCol, toCol, timer);
                 feedbackText.color = Color.Lerp(textFromCol, textToCol, timer);
                 timer += Time.deltaTime * speed;
                 yield return new WaitForEndOfFrame();
             } while (timer <= 1f);
             yield return new WaitForSeconds(2f);
-            yield return ResetColors();
+            yield return ResetPasswordColors();
         }
 
-        private IEnumerator ResetColors()
+        private IEnumerator ShowIncorrectNameAnim (string a_strErrorValue) {
+            float timer = 0f;
+            Color toCol = Helper.GetColorFromHex ("#E83811");
+            Color fromCol = m_refNameInputFieldBorder.color;
+            float speed = 3f;
+            feedbackText.text = a_strErrorValue;
+            feedbackText.color = new Color (0, 0, 0, 0);
+            Color textFromCol = feedbackText.color;
+            Color textToCol = Helper.GetColorFromHex ("#E83811");
+            textToCol.a = 1f;
+            do {
+                m_refNameInputFieldBorder.color = Color.Lerp (fromCol, toCol, timer);
+                feedbackText.color = Color.Lerp (textFromCol, textToCol, timer);
+                timer += Time.deltaTime * speed;
+                yield return new WaitForEndOfFrame ();
+            } while (timer <= 1f);
+            yield return new WaitForSeconds (2f);
+            yield return ResetNameColors ();
+        }
+
+        private IEnumerator ResetNameColors () {
+            float timer = 0f;
+            Color toCol = Color.white;
+            Color fromCol = m_refNameInputFieldBorder.color;
+            float speed = 3f;
+            Color textFromCol = feedbackText.color;
+            Color textToCol = new Color (0, 0, 0, 0);
+            textToCol.a = 1f;
+            do {
+                m_refNameInputFieldBorder.color = Color.Lerp (fromCol, toCol, timer);
+                feedbackText.color = Color.Lerp (textFromCol, textToCol, timer);
+                timer += Time.deltaTime * speed;
+                yield return new WaitForEndOfFrame ();
+            } while (timer <= 1f);
+            feedbackText.text = "";
+            feedbackText.color = new Color (0, 0, 0, 0);
+        }
+
+        private IEnumerator ResetPasswordColors()
         {
             float timer = 0f;
             Color toCol = Color.white;
-            Color fromCol = m_refInputFieldBorder.color;
+            Color fromCol = m_refPasswordInputFieldBorder.color;
             float speed = 3f;
             Color textFromCol = feedbackText.color;
             Color textToCol = new Color(0, 0, 0, 0);
             textToCol.a = 1f;
             do
             {
-                m_refInputFieldBorder.color = Color.Lerp(fromCol, toCol, timer);
+                m_refPasswordInputFieldBorder.color = Color.Lerp(fromCol, toCol, timer);
                 feedbackText.color = Color.Lerp(textFromCol, textToCol, timer);
                 timer += Time.deltaTime * speed;
                 yield return new WaitForEndOfFrame();
