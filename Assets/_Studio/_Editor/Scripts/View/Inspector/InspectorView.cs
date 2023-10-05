76f6914e1d0e8b73daa6599232b976aa7528f90f
AkshatTerra <akshatpuri@letsterra.com>
2023-10-05 18:15:44 +0530

    Akshat- wrong name reference fixed

--- Assets/_Studio/_Editor/Scripts/View/Inspector/InspectorView.cs
+++ Assets/_Studio/_Editor/Scripts/View/Inspector/InspectorView.cs
@@ -38,7 +38,7 @@ namespace Terra.Studio
         private void OnDestroy()
         {
             EditorOp.Unregister<InspectorView>(this);
-            EditorOp.Unregister<CopyPasteView>();
+            EditorOp.Unregister<CopyPasteSystem>();
         }
     }
 }
