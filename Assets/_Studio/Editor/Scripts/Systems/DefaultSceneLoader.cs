0af5d8d7fba849ea8f14db41b5a1c317e36f1398
Sujith:) <sujith@letsterra.com>
2023-10-19 19:11:17 +0530

    Sujith :) -> 1. If the bootstrap is already loaded, do not reload

--- Assets/_Studio/Editor/Scripts/Systems/DefaultSceneLoader.cs
+++ Assets/_Studio/Editor/Scripts/Systems/DefaultSceneLoader.cs
@@ -27,8 +27,18 @@ public static class DefaultSceneLoader
             var system = Resources.Load<SystemConfigurationSO>("System/SystemSettings");
             if (system.LoadDefaultSceneOnPlay)
             {
-                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
                 loadedDefaultScene = true;
+                var currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+                var scenesInBuildSettings = EditorBuildSettings.scenes;
+                if (scenesInBuildSettings != null && scenesInBuildSettings.Length > 0)
+                {
+                    if (scenesInBuildSettings[0].path.Contains(currentScene.name))
+                    {
+                        IsEnteringPlayerMode?.Invoke(loadedDefaultScene);
+                        return;
+                    }
+                }
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
                 IsEnteringPlayerMode?.Invoke(loadedDefaultScene);
             }
         }
